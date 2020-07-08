fab3c2cc5f674394d9ce0b8cb43796e82474d46a
zyf09 <zyf2992@163.com>
2020-07-08 18:46:19 +0800

    '初始化项目'

---
+++ src/components/decorate/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="decorate">
+    <div class="topBorder">
+      <span class="l mr" style="width:40px;" />
+      <span class="l mr" />
+      <span class="l mr" />
+      <span class="l mr" />
+      <span class="l mr" />
+      <span class="r ml" style="width:40px;" />
+      <span class="r ml" />
+      <span class="r ml" />
+      <span class="r ml" />
+      <span class="r ml" />
+    </div>
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script src="./index.js">
+</script>
+
+<style lang="less">
+@import './index.less';
+</style>
